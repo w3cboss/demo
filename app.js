391cbda14dddf9e9af5336938009773002e8ac26
lijiali <lijiali@henhaoji.com>
2018-05-18 17:16:22 +0800

    go on making structure

--- app.js
+++ app.js
@@ -2,19 +2,26 @@
 
 const koa = require('koa');
 const body = require('koa-bodyparser');
-const router = require('./router.js');
-const config = require('./lib/config');
+const uuid = require('uuid');
+
+const router = require('./router');
+const config = global.config = require('./lib/config');
+const logger = global.logger = require('./lib/logger');
 
 const app = new koa();
 
 app.use(body());
 
 app.use((ctx, next) => {
+  const __requestid = ctx.request.query.__requestid || ctx.request.body.__requestid ||
+    uuid.v1().replace(/\W/g, '');
+
   const params = Object.assign({}, ctx.request.query, ctx.request.body);
   ctx.params = params;
+  ctx.__requestid = __requestid;
 
   ctx.endfor = (code, msg, data) => {
-    ctx.body = { code, msg, data };
+    ctx.body = { code, msg, data, __requestid };
   };
 
   next();
--- controller/test.js
+++ controller/test.js
@@ -6,5 +6,7 @@ module.exports = {
 
 async function hi({ params, endfor }) {
   const { name } = params;
+
+  // ctx.body = 213123
   return endfor(0);
 }
